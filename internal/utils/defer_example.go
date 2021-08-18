9d5807a1e1517d17f83f227769da32b7aa3a595e
evyalyy <evyalyy@fusioncore.ru>
2021-08-18 22:09:43 +0300

    review fix

--- internal/utils/defer_example.go
+++ internal/utils/defer_example.go
@@ -2,16 +2,17 @@ package utils
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
-func OpenFileInCycle(path string) {
+func OpenFileInCycle(path string){
 	for i := 0; i < 10; i++ {
-		content := func() string {
+		content, err := func() (string, error) {
 			f, err := os.Open(path)
 			if err != nil {
 				fmt.Println("Error opening file:", err)
-				return ""
+				return "", err
 			}
 
 			defer func(f *os.File) {
@@ -21,15 +22,27 @@ func OpenFileInCycle(path string) {
 				}
 			}(f)
 
+			currContent := ""
 			tmp := make([]byte, 100)
-			numRead, err := f.Read(tmp)
-			if err != nil {
-				fmt.Println("Error while reading file:", err)
-				return ""
+			for {
+				_, err := f.Read(tmp)
+				if err != nil {
+					if err != io.EOF {
+						fmt.Println("Error while reading file:", err)
+						return currContent, err
+					}
+					break
+				}
+				currContent += string(tmp)
 			}
 
-			return string(tmp[:numRead])
+			return currContent, nil
 		}()
+
+		if err != nil {
+			fmt.Printf("Error when opening file: %s", err)
+			return
+		}
 		fmt.Printf("File content: `%s`\n", content)
 	}
 }
