08d6ac2732dab32ff4a2eba8d257f636bf7ea7ab
evyalyy <vyalyy.evgeny@gmail.com>
2021-08-21 16:02:03 +0300

    saver interface and impl

--- cmd/ova-recording-api/main.go
+++ cmd/ova-recording-api/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/ozonva/ova-recording-api/internal/flusher"
+	"github.com/ozonva/ova-recording-api/internal/repo"
+	"github.com/ozonva/ova-recording-api/internal/saver"
 	"github.com/ozonva/ova-recording-api/internal/utils"
+	"github.com/ozonva/ova-recording-api/pkg/recording"
+	"time"
 )
 
 
@@ -20,5 +25,19 @@ func main() {
 
 	fmt.Println(utils.FilterBy([]int{1,2,3,4,5,6}, []int{1,6,4}))
 
-	utils.OpenFileInCycle("/tmp/test.txt")
+	//utils.OpenFileInCycle("/tmp/test.txt")
+
+	dummyRepo := repo.NewDummyRepo()
+	fl := flusher.NewFlusher(3, dummyRepo)
+
+	s := saver.NewSaver(5, fl, 5)
+	s.Save(recording.Appointment{AppointmentID: 1})
+	s.Save(recording.Appointment{AppointmentID: 2})
+	time.Sleep(time.Second * 3)
+	s.Save(recording.Appointment{AppointmentID: 3})
+	s.Save(recording.Appointment{AppointmentID: 4})
+	time.Sleep(time.Second * 3)
+	s.Save(recording.Appointment{AppointmentID: 5})
+	s.Save(recording.Appointment{AppointmentID: 6})
+	s.Close()
 }
---
+++ internal/saver/saver.go
@@ -0,0 +1,84 @@
+package saver
+
+import (
+	"fmt"
+	"github.com/ozonva/ova-recording-api/internal/flusher"
+	"github.com/ozonva/ova-recording-api/pkg/recording"
+	"sync"
+	"time"
+)
+
+
+type Saver interface {
+	Save(entity recording.Appointment)
+	Close()
+}
+
+
+type saver struct {
+	entities []recording.Appointment
+	fl flusher.Flusher
+	m sync.Mutex
+	saveInterval int
+	doneCh chan bool
+}
+
+func NewSaver(capacity int, fl flusher.Flusher, saveInterval int) Saver {
+	s := &saver{
+		entities: make([]recording.Appointment, 0, capacity),
+		fl: fl,
+		saveInterval: saveInterval,
+		doneCh: make(chan bool),
+	}
+
+	s.init()
+
+	return s
+}
+
+func (s* saver) Save(entity recording.Appointment) {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if len(s.entities) == cap(s.entities) {
+		s.flush()
+	}
+
+	s.entities = append(s.entities, entity)
+}
+
+func (s* saver) Close() {
+	s.doneCh <- true
+	close(s.doneCh)
+
+	s.flush()
+}
+
+func (s *saver) flush() {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	fmt.Println("Going to flush", len(s.entities), "entities")
+
+	s.fl.Flush(s.entities)
+
+	s.entities = s.entities[:0]
+}
+
+func (s *saver) init() {
+	go func() {
+		ticker := time.NewTicker(time.Second * time.Duration(s.saveInterval))
+		defer ticker.Stop()
+		for {
+			select {
+				case <-s.doneCh:
+					fmt.Println("Closing ticking goroutine")
+					return
+				case <-ticker.C:
+					fmt.Println("Tick, saving")
+					s.flush()
+			}
+		}
+
+	}()
+}
